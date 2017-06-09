d9d509e6bd32673a66c43a27a206a3c78856755f
Kuchuk Andrey <kreezag@gmail.com>
2017-06-09 09:05:21 +0300

    add store to form

--- index.js
+++ index.js
@@ -1,11 +1,71 @@
 'use strict';
 
 import React from 'react';
+import { createStore } from 'redux';
 import { render } from 'react-dom';
 
+const LOAD_NEW_PASSWORD = 'LOAD_NEW_PASSWORD';
+
+const defaultPassword = 'myPassword';
+
+
+const initialState = {
+    currentPassword: defaultPassword
+};
+
+
+//action creator
+const actionCreator = (currentPassword) => ({
+    type: LOAD_NEW_PASSWORD,
+    currentPassword
+});
+
+//action
+const defaultAction = actionCreator(defaultPassword);
+
+
+// reducer
+const setNewPassword = (state = initialState, action) => {
+    if (action.type === LOAD_NEW_PASSWORD) {
+        return Object.assign({}, state, action)
+    }
+
+    return state;
+};
+
+//store
+let store = createStore(setNewPassword, defaultAction);
+
+
+
+const newAction = actionCreator('newPassword');
+
+store.dispatch(newAction);
+
+
+
+
 const App = () => (
-    <div>Hello!</div>
+    <div>
+        <div className="appHeader">My form</div>
+        <form className="myForm">
+            <label className="myLabel">
+            Set current password
+                <input className="myInput" type="text" name="password"/>
+            </label>
+            <label className="myLabel">
+            Set new password
+                <input className="myInput" type="text" name="password"/>
+            </label>
+            {/*{ errors && (<div className="myFormErrors">{errors}</div>)}*/}
+            <button className="myButton">Change password</button>
+        </form>
+    </div>
 );
+
+// App.propTypes = {
+//     onChangeValue: PropTypes.func
+// };
 
 render((<App/>),
     document.getElementById('react-root')
